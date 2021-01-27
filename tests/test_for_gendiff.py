c215cedc1ee773068c9b29c889fd2224c7605a25
Data-Wan <ustimenko.work@gmail.com>
2021-01-27 17:31:26 +0300

    test_for_gendiff.py moved example for separate file

--- tests/test_for_gendiff.py
+++ tests/test_for_gendiff.py
@@ -1,15 +1,7 @@
 """File for test gendiff."""
 from calculate_diff.modules.generate_gendiff import generate_diff
 
-example = """{
-  - follow: False
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: True
-}"""
-
 
 def test_flat_files():
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == example
+    with open('tests/fixtures/check_for_f1_f2.txt') as example:
+        assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == example.read()
