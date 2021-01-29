c1424a00ffde6f7a954f0ab4ed31d8ef84536b56
Data-Wan <ustimenko.work@gmail.com>
2021-01-29 21:16:54 +0300

    fix module for gendiff

--- calculate_diff/scripts/gendiff.py
+++ calculate_diff/scripts/gendiff.py
@@ -5,18 +5,19 @@
 import argparse
 
 from calculate_diff.modules.generate_diff import generate_diff
+from calculate_diff.modules.stulish_funcs import stylish
 
 parser = argparse.ArgumentParser()
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
-parser.add_argument('-f', '--format', help='set format of output')
+parser.add_argument('-f', '--format', default=stylish, help='set format of output')
 
 args = parser.parse_args()
 
 
 def main():
     """Print generate diff for 2 json files."""
-    print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
