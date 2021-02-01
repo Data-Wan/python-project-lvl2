4a1ca51252a423c9373fb92048587144b9dfb506
Data-Wan <ustimenko.work@gmail.com>
2021-02-01 18:14:28 +0300

    fix default config

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,13 +4,10 @@
 
 import argparse
 
-from gendiff.formatters.jsonify_formatter import jsonify  # noqa: F401
-from gendiff.formatters.plain_formatter import plain  # noqa: F401
-from gendiff.formatters.stylish_formatter import stylish
 from gendiff.gendiff import generate_diff
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--format', default=stylish, help='set format of output')  # noqa: E501
+parser.add_argument('-f', '--format', default='stylish', help='set format of output')  # noqa: E501
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
 
